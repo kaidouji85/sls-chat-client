dc21676995725d908d46b31a57e6d7f5bf18c5f0
Yuusuke <kaidouji85@gmail.com>
2021-08-10 09:56:25 +0000

    リファクタリングした

--- src/index.js
+++ src/index.js
@@ -3,29 +3,60 @@
 import createAuth0Client from '@auth0/auth0-spa-js';
 import {Auth0Client} from '@auth0/auth0-spa-js';
 
-let auth0: ?(typeof Auth0Client) = null;
-let socket: ?WebSocket = null;
-
-const loginButton = document.querySelector('#login-button') ?? document.createElement('button');
-const logoffButton = document.querySelector('#logoff-button') ?? document.createElement('button');
-
-window.addEventListener('load', async (): Promise<void> => {
-  auth0 = await createAuth0Client({
+/**
+ * auth0 clientを生成するヘルパー関数
+ * 
+ * @returns auth0 client
+ */
+function createAuth0ClientHelper(): Promise<typeof Auth0Client> {
+  return createAuth0Client({
     domain: process.env.AUTH0_DOMAIN,
     client_id: process.env.AUTH0_CLIENT_ID,
     responseType: 'token id_token',
     audience: process.env.AUTH0_AUDIENCE,
     redirectUri: window.location.origin,
   });
+}
 
-  loginButton.addEventListener('click', onLoginButtonClick);
-  logoffButton.addEventListener('click', onLogoffButtonClick);
-
+/**
+ * ログイン成功時のリダイレクトか否かを判定する
+ * trueでログイン成功時のリダイレクトである
+ * 
+ * @return 判定結果
+ */
+function isLoginSuccessRedirect(): boolean {
   const query = window.location.search;
-  if (query.includes("code=") && query.includes("state=")) {
+  return query.includes("code=") && query.includes("state=");
+}
+
+/**
+ * ログインリダイレクトのヒストリーをクリアする
+ */
+function clearLoginHistory(): void {
+  window.history.replaceState({}, document.title, "/");
+}
+
+/**
+ * エントリポイント
+ */
+window.addEventListener('load', async (): Promise<void> => {
+  const auth0 = await createAuth0ClientHelper();
+  if (isLoginSuccessRedirect()) {
     await auth0.handleRedirectCallback();
-    window.history.replaceState({}, document.title, "/");
+    clearLoginHistory();
   }
+
+  const loginButton = document.querySelector('#login-button') 
+    ?? document.createElement('button');
+  loginButton.addEventListener('click', async (): Promise<void> => {
+    await auth0.loginWithRedirect({redirect_uri: window.location.origin});
+  });
+  
+  const logoffButton = document.querySelector('#logoff-button')
+    ?? document.createElement('button');
+  logoffButton.addEventListener('click', () => {
+    auth0.logout({returnTo: window.location.origin});
+  });
 
   const isAuthenticated = await auth0.isAuthenticated();
   if (!isAuthenticated) {
@@ -35,25 +66,5 @@ window.addEventListener('load', async (): Promise<void> => {
 
   const accessToken = await auth0.getTokenSilently();
   const websocketURL = process.env.SLS_CAHT_API_URL ?? '';
-  socket = new WebSocket(`${websocketURL}?token=${accessToken}`);
+  new WebSocket(`${websocketURL}?token=${accessToken}`);  // TODO socketを利用する
 });
-
-async function onLoginButtonClick() {
-  if (!auth0) {
-    return;
-  }
-
-  await auth0.loginWithRedirect({
-    redirect_uri: window.location.origin
-  });
-}
-
-function onLogoffButtonClick() {
-  if (!auth0) {
-    return;
-  }
-
-  auth0.logout({
-    returnTo: window.location.origin
-  });
-}
